db209bbef5ef2dad710035c95d904a861b0bb867
Unknown <coreyog@gmail.com>
2018-06-26 09:37:03 -0600

    Initial code check in

---
+++ main.go
@@ -0,0 +1,204 @@
+//go:generate go-bindata -o embedded/embedded.go -pkg embedded assets/...
+package main
+
+import (
+	"fmt"
+
+	"bitbucket.org/coreyog/rubixtimer/embedded"
+	"bitbucket.org/coreyog/rubixtimer/util"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
+	"github.com/faiface/pixel/pixelgl"
+	"github.com/faiface/pixel/text"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/colornames"
+	"golang.org/x/image/font"
+)
+
+var sevenSegBigAtlas *text.Atlas
+var sevenSegSmallAtlas *text.Atlas
+
+type state int
+
+const (
+	stateWaitingForHold state = iota
+	stateWaitingForRelease
+	stateRunning
+	stateDone
+)
+
+var currentState = stateWaitingForHold
+
+func main() {
+	fmt.Print("loading font...")
+	sevenSegBigFont, err := loadTTF("assets/DSEG7Modern-Bold.ttf", 200)
+	if err != nil {
+		fmt.Println()
+		panic(err)
+	}
+	sevenSegSmallFont, err := loadTTF("assets/DSEG7Modern-Bold.ttf", 100)
+	if err != nil {
+		fmt.Println()
+		panic(err)
+	}
+	sevenSegBigAtlas = text.NewAtlas(sevenSegBigFont, text.ASCII)
+	sevenSegSmallAtlas = text.NewAtlas(sevenSegSmallFont, text.ASCII)
+	fmt.Print("DONE")
+	pixelgl.Run(run)
+}
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Rubix Timer",
+		Bounds: pixel.R(0, 0, 1000, 400),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+	win.SetSmooth(true)
+
+	bigSeven := text.New(pixel.V(0, 0), sevenSegBigAtlas)
+	smallSeven := text.New(pixel.V(0, 0), sevenSegSmallAtlas)
+
+	yellowIndicator := imdraw.New(nil)
+	yellowIndicator.Color = colornames.Yellow
+	immediatePill(yellowIndicator, win)
+
+	greenIndicator := imdraw.New(nil)
+	greenIndicator.Color = colornames.Lime
+	immediatePill(greenIndicator, win)
+
+	dt := util.NewDeltaTimer(0)
+	elapsed := float64(0)
+	backgroundFlip := true
+	flipPressed := false
+
+	for !win.Closed() {
+		if backgroundFlip {
+			win.Clear(colornames.Black)
+		} else {
+			win.Clear(colornames.Magenta)
+		}
+		dt.Tick()
+		t := dt.SinceLastFrame()
+
+		if win.Pressed(pixelgl.KeyF12) {
+			if !flipPressed {
+				backgroundFlip = !backgroundFlip
+				flipPressed = true
+			}
+		} else {
+			flipPressed = false
+		}
+
+		if win.Pressed(pixelgl.KeyEscape) {
+			win.SetClosed(true)
+		}
+
+		switch currentState {
+		case stateWaitingForHold:
+			yellowIndicator.Draw(win)
+			if win.Pressed(pixelgl.KeyLeftControl) && win.Pressed(pixelgl.KeyRightControl) {
+				currentState = stateWaitingForRelease
+			}
+			break
+		case stateWaitingForRelease:
+			if blink(dt) {
+				yellowIndicator.Draw(win)
+			}
+			if !win.Pressed(pixelgl.KeyLeftControl) || !win.Pressed(pixelgl.KeyRightControl) {
+				currentState = stateRunning
+			}
+			break
+		case stateRunning:
+			elapsed += t
+			greenIndicator.Draw(win)
+			if win.Pressed(pixelgl.KeyLeftControl) && win.Pressed(pixelgl.KeyRightControl) {
+				currentState = stateDone
+			}
+		case stateDone:
+			if blink(dt) {
+				greenIndicator.Draw(win)
+			}
+			if win.Pressed(pixelgl.KeyR) {
+				elapsed = 0
+				currentState = stateWaitingForHold
+			}
+		}
+
+		minsec, milli := buildTimer(elapsed)
+		bigSeven.Clear()
+		smallSeven.Clear()
+
+		fmt.Fprint(bigSeven, minsec)
+		fmt.Fprint(smallSeven, milli)
+
+		offCenter := bigSeven.Bounds().Center().Scaled(-1)
+		offCenter.X -= 130
+		mat := pixel.IM.Moved(win.Bounds().Center()).Moved(offCenter)
+		bigSeven.Draw(win, mat)
+		mat = mat.Moved(pixel.V(bigSeven.Bounds().W()+30, 0))
+		smallSeven.Draw(win, mat)
+		win.Update()
+	}
+}
+
+func buildTimer(t float64) (minsec string, milli string) {
+	// return "01234:56789"
+	minutes := int(t) / 60
+	seconds := int(t) % 60
+	return fmt.Sprintf("%02d:%02d", minutes, seconds), fmt.Sprintf(".%03d", int(t*1000)%1000)
+}
+
+func blink(dt *util.DeltaTimer) bool {
+	return (int(dt.TotalTime()*1000)/550)%2 == 1
+}
+
+func immediatePill(imd *imdraw.IMDraw, win *pixelgl.Window) {
+	pt := win.Bounds().Center()
+	pt.Y -= 150
+	pt.X -= 125
+
+	// lower left
+	pt.X -= 100
+	pt.Y -= 25
+	imd.Push(pt)
+	// lower right
+	pt.X += 200
+	imd.Push(pt)
+	// upper right
+	pt.Y += 50
+	imd.Push(pt)
+	// upper left
+	pt.X -= 200
+	imd.Push(pt)
+	imd.Polygon(0)
+
+	// left cap
+	pt.Y -= 25
+	imd.Push(pt)
+	imd.Circle(25, 0)
+	// right cap
+	pt.X += 200
+	imd.Push(pt)
+	imd.Circle(25, 0)
+}
+
+func loadTTF(path string, size float64) (font.Face, error) {
+	rawFont, err := embedded.Asset(path)
+	if err != nil {
+		return nil, err
+	}
+	ttfont, err := truetype.Parse(rawFont)
+	if err != nil {
+		return nil, err
+	}
+
+	return truetype.NewFace(ttfont, &truetype.Options{
+		Size:    size,
+		Hinting: font.HintingFull,
+	}), nil
+}
---
+++ util/deltatimer.go
@@ -0,0 +1,63 @@
+package util
+
+import (
+	"time"
+)
+
+// DeltaTimer manages TotalTime and LastFrame time
+type DeltaTimer struct {
+	fixedRate uint
+	tickCount uint
+	startTime time.Time
+	lastFrame time.Time
+
+	totalTime      float64
+	sinceLastFrame float64
+}
+
+// NewDeltaTimer starts a new DeltaTimer
+func NewDeltaTimer(fixedRate uint) (dt *DeltaTimer) {
+	dt = new(DeltaTimer)
+	dt.fixedRate = fixedRate
+	dt.start()
+	return dt
+}
+
+func (dt *DeltaTimer) start() {
+	dt.startTime = time.Now()
+	dt.lastFrame = dt.startTime
+}
+
+// Tick is called at the first of every time to make timing measurements
+func (dt *DeltaTimer) Tick() {
+	dt.tickCount++
+	if dt.fixedRate == 0 {
+		now := time.Now()
+		dt.sinceLastFrame = now.Sub(dt.lastFrame).Seconds()
+		dt.totalTime = now.Sub(dt.startTime).Seconds()
+		dt.lastFrame = now
+	} else {
+		dt.sinceLastFrame = 1 / float64(dt.fixedRate)
+		dt.totalTime = float64(dt.tickCount) / float64(dt.fixedRate)
+	}
+}
+
+// TotalTime returns the time since the DeltaTimer was created
+func (dt *DeltaTimer) TotalTime() (seconds float64) {
+	return dt.totalTime
+}
+
+// SinceLastFrame returns the time since the last frame
+func (dt *DeltaTimer) SinceLastFrame() (seconds float64) {
+	return dt.sinceLastFrame
+}
+
+// IsFixed returns if ticks count time in realtime or at a fixed rate
+func (dt *DeltaTimer) IsFixed() (fixed bool) {
+	return dt.fixedRate != 0
+}
+
+// TickCount returns how many ticks have passed
+func (dt *DeltaTimer) TickCount() (count uint) {
+	return dt.tickCount
+}
